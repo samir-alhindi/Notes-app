0b215f7071b362ad1c498c8374676ade467a31a4
buzzbuzz20xx <samirskyland@gmail.com>
2025-11-20 14:33:34 +0300

    pressing the back button now saves the note + removed ugly black borders from notes screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: const HomeScreen(),
     )
 );
@@ -34,21 +35,22 @@ class _HomeScreenState extends State<HomeScreen> {
       backgroundColor: Colors.yellow,
     ),
 
-    body: notes.isEmpty ? Center(child: Text("No notes added yet", style: TextStyle(fontSize: 25),)) :
+    body: notes.isEmpty ? Center(child: Text("No notes added yet", style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),)) :
 
     ListView.separated(
       padding: EdgeInsets.all(8.0),
       itemCount: notes.length,
       separatorBuilder: (BuildContext context, int index) => SizedBox(height: 10),
-      itemBuilder: (context, index) => ListTile(
-        title: Text(notes[index].title),
-        trailing: IconButton(onPressed: ()=>setState(()=>notes.removeAt(index)), icon: Icon(Icons.delete)),
-        onTap: () async {
-          Note result = await Navigator.push(context, MaterialPageRoute(builder: (newContext) => NoteScreen(note: notes[index])));
-          setState(()=>notes[index] = result);
-          } ,
-        shape: RoundedRectangleBorder(side: BorderSide(color: Colors.black), borderRadius: BorderRadiusGeometry.circular(12)),
-        ),
+      itemBuilder: (context, index) => Card(
+        child: ListTile(
+          title: Text(notes[index].title),
+          trailing: IconButton(onPressed: ()=>setState(()=>notes.removeAt(index)), icon: Icon(Icons.delete)),
+          onTap: () async {
+            Note result = await Navigator.push(context, MaterialPageRoute(builder: (newContext) => NoteScreen(note: notes[index])));
+            setState(()=>notes[index] = result);
+            } ,
+          ),
+      ),
     ),
 
 
@@ -77,51 +79,51 @@ class _NoteScreenState extends State<NoteScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (didPop, result) => didPop ?  null : Navigator.pop(context, widget.note),
+      child: Scaffold(
 
-    appBar: AppBar(
-      backgroundColor: Colors.yellow,
-      leading: IconButton(
-          icon: Icon(Icons.check),
-          onPressed: () => Navigator.pop(context, widget.note),
-      )
-    ),
-
-    body: Container(
-      margin: EdgeInsets.all(15),
-      child: Column(
-
-        children: [
-
-          TextFormField(
-            initialValue: widget.note.title,
-            autofocus: true,
-            autocorrect: true,
-            textCapitalization: TextCapitalization.sentences,
-            maxLength: 100,
-            decoration: InputDecoration(
-                hint: Text("Note title here", style: TextStyle(
-                    color: Colors.grey.withAlpha(125)
-                ),
-                )
-            ),
-            onChanged: (title) => setState(()=> widget.note.title = title),
-          ),
-
-          TextFormField(
-            initialValue: widget.note.body,
-            keyboardType: TextInputType.multiline,
-            decoration: InputDecoration(
-              hint: Text("Note body here", style: TextStyle(
-                  color: Colors.grey.withAlpha(125)
-            ))),
-            maxLines: null,
-            autocorrect: true,
-            textCapitalization: TextCapitalization.sentences,
-            onChanged: (body) => setState(()=> widget.note.body = body)
-          ),
-        ],
+      appBar: AppBar(
+        backgroundColor: Colors.yellow,
       ),
-    )
-  );}
+
+      body: Container(
+        margin: EdgeInsets.all(15),
+        child: Column(
+
+          children: [
+
+            TextFormField(
+              initialValue: widget.note.title,
+              autofocus: true,
+              autocorrect: true,
+              textCapitalization: TextCapitalization.sentences,
+              maxLength: 100,
+              decoration: InputDecoration(
+                  hint: Text("Note title here", style: TextStyle(
+                      color: Colors.grey.withAlpha(125)
+                  ),
+                  )
+              ),
+              onChanged: (title) => setState(()=> widget.note.title = title),
+            ),
+
+            TextFormField(
+              initialValue: widget.note.body,
+              keyboardType: TextInputType.multiline,
+              decoration: InputDecoration(
+                hint: Text("Note body here", style: TextStyle(
+                    color: Colors.grey.withAlpha(125)
+              ))),
+              maxLines: null,
+              autocorrect: true,
+              textCapitalization: TextCapitalization.sentences,
+              onChanged: (body) => setState(()=> widget.note.body = body)
+            ),
+          ],
+        ),
+      )
+        ),
+    );}
 }
